ec57783b66e7a813ed2be2d28a7667c35a6aa8ce
Phil Egelston <phil.egelston@trustedchoice.com>
2019-05-30 15:06:45 -0500

    tweak format

--- find_fks.sql
+++ find_fks.sql
@@ -2,8 +2,7 @@ SELECT tc.table_schema
      , tc.constraint_name
      , tc.table_name
      , kcu.column_name
-     , ccu.table_name
-                       AS foreign_table_name
+     , ccu.table_name  AS foreign_table_name
      , ccu.column_name AS foreign_column_name
 FROM
     information_schema.table_constraints                tc
@@ -11,4 +10,5 @@ FROM
         JOIN information_schema.constraint_column_usage ccu ON ccu.constraint_name = tc.constraint_name
 WHERE
       constraint_type = 'FOREIGN KEY'
-  AND ccu.table_name = 'ic_code'
+  AND ccu.table_name = '<table_name>'
+;
