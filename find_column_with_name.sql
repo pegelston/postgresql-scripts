ad12aa223c4085a41dd415e653553f5fd9bd2199
Phil Egelston <phil.egelston@trustedchoice.com>
2019-08-02 10:31:38 -0500

    adding more utils

---
+++ find_column_with_name.sql
@@ -0,0 +1,12 @@
+SELECT t.table_schema
+     , t.table_name
+FROM
+    information_schema.tables                 t
+        INNER JOIN information_schema.columns c ON c.table_name = t.table_name
+        AND c.table_schema = t.table_schema
+WHERE
+      c.column_name = '<column_name>'
+  AND t.table_schema NOT IN ('information_schema', 'pg_catalog')
+  AND t.table_type = 'BASE TABLE'
+ORDER BY t.table_schema
+;
